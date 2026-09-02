3af91672880f036c9cdd4ac57816ce582fe3ccc4
Veralucia0909 <verinha539@hotmail.com>
2026-09-01 21:35:03 -0300

    get, post, put e delete

--- api_clinica/src/app.ts
+++ api_clinica/src/app.ts
@@ -11,21 +11,67 @@ app.get('/usuarios', async (req: Request, res: Response) => {
   res.json(users);
 });
 
-app.post('/usuario', async (req: Request, res: Response) => {
-  const body = req.body
-  const hashSenha =  await bcrypt.hash(body.password, 100)
-  const usuario = await db.orm.public.User.create({
+app.get('/usuarios/:id', async (req: Request, res: Response) => {
+  const id = Number(req.params.id)
+  const usuario = await db.orm.public.User.where({ id }).first()
+  if(usuario){
+    res.json(usuario);
+  } else {
+    res.status(400).send("Usuário não existe!")
+  }
+});
+
+app.put('/usuarios/:id', async (req: Request, res: Response) => {
+  const body = req.body;
+  const id = Number(req.params.id)
+
+  const hashSenha = await bcrypt.hash(body.password, 10)
+  const usuario = await db.orm.public.User.where({ id }).update({
     email: body.email,
     name: body.name,
     password: hashSenha,
     username: body.username
   });
+  if (usuario) {
+    res.json({
+      "mensagem": "Usuário atualizado com sucesso!",
+      "data": usuario
+    });
+  }
+  res.status(400).send("Usuário não encontrado!")
+
+});
+
+app.delete('/usuarios/:id', async (req: Request, res: Response) => {
+  const id = Number(req.params.id)
+  const usuario = await db.orm.public.User.where({ id }).delete()
+  if (usuario) {
+    res.json({
+      mensagem: "Usuário deletado",
+      data: usuario
+    });
+  } else {
+    res.status(400).send("Usuário não existe!")
+  }
+});
+
+app.post('/usuario', async (req: Request, res: Response) => {
+  const body = req.body
+  const hashSenha =  await bcrypt.hash(body.password, 10)
+  const usuario = await db.orm.public.User.create({
+    email: body.email,
+    name: body.name,
+    password: hashSenha,
+    username: body.username
+  });5
 
   res.json({
     "mensagem": "Usuário Criado!",
     "data": usuario
   });
 });
+
+
 
 app.listen(3000, () => {
   console.log("Rodando em http://localhost:3000")
